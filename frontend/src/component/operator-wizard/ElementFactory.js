14f115436dff9fd943733c0020a0a2d0ebded9fc
Ricardo Zanini <zanini@redhat.com>
2019-04-24 10:48:50 -0300

    factory documentation

--- frontend/src/component/operator-wizard/ElementFactory.js
+++ frontend/src/component/operator-wizard/ElementFactory.js
@@ -4,10 +4,13 @@ import { ButtonFactory } from "./button-factory/ButtonFactory";
 export class ElementFactory {
   /**
    * Creates an array of element instances to be populated on each step
+   * @param {*} fieldDefs the field defined by the JSON loaded into mem
+   * @param {*} buttonDefs the button defined by the JSON loaded into mem
+   * @param {*} jsonSchema the JSON Schema loaded into mem
+   * @param {int} pageNumber the Page/Step number
    */
   static newInstances(fieldDefs, buttonDefs, jsonSchema, pageNumber) {
     const children = [];
-
     children.push(FieldFactory.newInstances(fieldDefs, jsonSchema, pageNumber));
     children.push(ButtonFactory.newInstances(buttonDefs, pageNumber));
     return children;
